7ef87a85a19a0d34b6a085bbf23b32d1e6d6d3aa
jlafferton <jan.lafferton@vector.com>
2023-12-01 14:14:48 +0100

    Improve

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,28 @@
-pub fn trebuchet_part1(input: &str) -> u32 {
+pub fn day1_part1() -> u32 {
+    let input = std::fs::read_to_string("inputs/day1.txt").unwrap();
     input.lines().fold(0, |result, line| {
         let iter = line.chars().filter_map(|c| c.to_digit(10));
         result + iter.clone().next().unwrap() * 10 + iter.last().unwrap()
     })
 }
 
-pub fn trebuchet_part2(input: &str) -> u32 {
+pub fn day1_part2() -> u32 {
+    let input = std::fs::read_to_string("inputs/day1.txt").unwrap();
     input.lines().fold(0, |result, line| {
         let chars = line.chars().collect::<Vec<char>>();
-        let iter = chars
-            .iter()
-            .enumerate()
-            .filter_map(|(idx, _)| match &chars[idx..line.len()] {
-                ['0', ..] | ['z', 'e', 'r', 'o', ..] => Some(0),
-                ['1', ..] | ['o', 'n', 'e', ..] => Some(1),
-                ['2', ..] | ['t', 'w', 'o', ..] => Some(2),
-                ['3', ..] | ['t', 'h', 'r', 'e', 'e', ..] => Some(3),
-                ['4', ..] | ['f', 'o', 'u', 'r', ..] => Some(4),
-                ['5', ..] | ['f', 'i', 'v', 'e', ..] => Some(5),
-                ['6', ..] | ['s', 'i', 'x', ..] => Some(6),
-                ['7', ..] | ['s', 'e', 'v', 'e', 'n', ..] => Some(7),
-                ['8', ..] | ['e', 'i', 'g', 'h', 't', ..] => Some(8),
-                ['9', ..] | ['n', 'i', 'n', 'e', ..] => Some(9),
-                _ => None,
-            });
+        let iter = (0..chars.len()).filter_map(|idx| match &chars[idx..chars.len()] {
+            ['0', ..] | ['z', 'e', 'r', 'o', ..] => Some(0),
+            ['1', ..] | ['o', 'n', 'e', ..] => Some(1),
+            ['2', ..] | ['t', 'w', 'o', ..] => Some(2),
+            ['3', ..] | ['t', 'h', 'r', 'e', 'e', ..] => Some(3),
+            ['4', ..] | ['f', 'o', 'u', 'r', ..] => Some(4),
+            ['5', ..] | ['f', 'i', 'v', 'e', ..] => Some(5),
+            ['6', ..] | ['s', 'i', 'x', ..] => Some(6),
+            ['7', ..] | ['s', 'e', 'v', 'e', 'n', ..] => Some(7),
+            ['8', ..] | ['e', 'i', 'g', 'h', 't', ..] => Some(8),
+            ['9', ..] | ['n', 'i', 'n', 'e', ..] => Some(9),
+            _ => None,
+        });
         result + iter.clone().next().unwrap() * 10 + iter.last().unwrap()
     })
 }
@@ -33,14 +32,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn day1_part1() {
-        let input = std::fs::read_to_string("inputs/day1.txt").unwrap();
-        println!("Result of challenge: {}", trebuchet_part1(&input));
+    fn day1_part1_test() {
+        println!("Result of challenge: {}", day1_part1());
     }
 
     #[test]
-    fn day1_part2() {
-        let input = std::fs::read_to_string("inputs/day1.txt").unwrap();
-        println!("Result of challenge: {}", trebuchet_part2(&input));
+    fn day1_part2_test() {
+        println!("Result of challenge: {}", day1_part2());
     }
 }
